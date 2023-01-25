9f85646fba8b0689df0edda6000644881c2b8d6b
Mashood ur Rehman Rana <ranamashood@proton.me>
2023-01-25 17:45:33 +0500

    WIP updated button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { NoteAdd } from "@styled-icons/material";
 
 interface Props {
   title: string;
@@ -9,9 +10,41 @@ interface Props {
 const Button = (props: Props) => {
   const { title, onClick } = props;
 
-  return <StyledButton onClick={onClick}>{title}</StyledButton>;
+  return (
+    <StyledButton onClick={onClick}>
+      <NoteAddIcon />
+      <Span>{title}</Span>
+    </StyledButton>
+  );
 };
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button`
+  border: none;
+  outline: none;
+  border-radius: ${(props) => props.theme.borderRadius};
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  float: right;
+
+  &:hover {
+    cursor: pointer;
+    & span {
+      max-width: 200px;
+    }
+  }
+`;
+
+const NoteAddIcon = styled(NoteAdd)`
+  width: ${(props) => props.theme.fontSizes.medium};
+`;
+
+const Span = styled.span`
+  font-size: ${(props) => props.theme.fontSizes.medium};
+  overflow: hidden;
+  max-width: 0;
+  white-space: nowrap;
+  transition: max-width 500ms;
+`;
 
 export default Button;
